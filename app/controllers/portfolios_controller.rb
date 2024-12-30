579cc45a9d79f9ab99ecc28e7b0b3cb20bca7e80
notmoeez <abdulmoezz321@gmail.com>
2024-12-30 16:02:15 +0500

    integrated update functionalty

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params.expect(:id))
+  end
+
   def create
     @portfolio_item = Portfolio.new(params.expect(portfolio: [ :title, :subtitle, :body ]))
 
@@ -16,6 +20,19 @@ class PortfoliosController < ApplicationController
         format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params.expect(:id))
+    respond_to do |format|
+      if @portfolio_item.update(params.expect(portfolio: [ :title, :subtitle, :body ]))
+        format.html { redirect_to portfolios_url, notice: "Portfolio Item was successfully updated." }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
