56b4bd8d7ca0e37371d3f92f5ad7083badf9c1d0
notmoeez <abdulmoezz321@gmail.com>
2024-12-30 19:43:18 +0500

    integrated friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -60,7 +60,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params.expect(:id))
+      @blog = Blog.friendly.find(params.expect(:id))
     end
 
     # Only allow a list of trusted parameters through.
