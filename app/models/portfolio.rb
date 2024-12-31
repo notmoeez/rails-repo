82dc1e1840bb242b6948dfc97cd7df137d87ecb9
notmoeez <abdulmoezz321@gmail.com>
2024-12-31 18:51:43 +0500

    implemented concerns for image generator method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :subtitle, :body, :thumb_image, :main_image
 
   def self.search_by_subtopic(subtopic)
@@ -10,7 +11,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://placehold.co/600x400"
-    self.thumb_image ||= "https://placehold.co/300x200"
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height: "300", width: "200")
   end
 end
