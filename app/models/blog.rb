6232c75c08954d530e6b9d04fb143987892ff365
notmoeez <abdulmoezz321@gmail.com>
2024-12-31 16:10:24 +0500

    added validations for models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ApplicationRecord
   enum :posts_status, { post_unpublished: 0, post_published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
 end
