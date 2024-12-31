dbdf2366a1d35469b204e79c9fb9da188ab6cf28
notmoeez <abdulmoezz321@gmail.com>
2024-12-31 17:42:43 +0500

    integrated relattioship between blogs and topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_31_075723) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_31_123048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,7 +21,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_075723) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "posts_status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -51,4 +53,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_075723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
